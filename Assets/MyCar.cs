fdec854d64f795db4fb68ca547a150d21446bf6e
Jan Zelinka <jan.zelinka12@gmail.com>
2025-01-18 15:15:35 +0100

    init commit

---
+++ Assets/MyCar.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class MyCar
+{
+    private void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        // animator.
+    }
+}
